9df9877092e57918eaddad793028c1a2f84caa3d
Ahmed Ahmed <afaraj@gmail.com>
2016-02-10 20:41:33 -0800

    messed around

--- gp.php
+++ gp.php
@@ -9,8 +9,45 @@
 
     <body>
         <?php
-            $name = filter_input(INPUT_GET, firstname);
-            print "<h1>Hello, $name!</h1>";
+        // Load user values
+        $first = filter_input(INPUT_POST, "firstname");
+        $last  = filter_input(INPUT_POST, "lastname");
+        $email = filter_input(INPUT_POST, "email");
+        $company  = filter_input(INPUT_POST, "company");
+
+        
+        try{
+            $con = new PDO("mysql:host=localhost;dbname=conctact", "root", PASSWORD);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                // Constrain the query if we got first and last names.
+                if ((strlen($first) > 0) && (strlen($last) > 0)) {
+                $query = "SELECT * FROM contactinfo ".
+                "WHERE email='$email'";
+                }
+
+                // Fetch the matching database table rows.
+                $data = $con->query($query);
+                $data->setFetchMode(PDO::FETCH_ASSOC);
+
+                // Construct the HTML table row by row.
+                foreach ($data as $row) {
+                    print "<tr>\n";
+                    foreach ($row as $name => $value) {
+                        print "<td>$value</td>\n";
+                    }
+
+                print "
+            </tr>\n";
+            }
+
+            print "</table>\n";
+        }
+        catch(PDOException $ex) {
+        echo 'ERROR: '.$ex->getMessage();
+        }
+
+
+
         ?>
     </body>
 </html>
